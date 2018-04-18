ec0eff51d197f486e751c21a1a3a52042beb08af
oldmannt <oldman.mtb@gmail.com>
2018-04-17 23:47:15 -0700

    file uploads, with mistake

--- meadowlark/site/meadowlark.js
+++ meadowlark/site/meadowlark.js
@@ -164,6 +164,33 @@ app.post('/process', function(req, res){
 	}
 });
 
+// file uploads
+var formidable = require('formidable')
+
+app.get('/contest/vacation-photo', function(req, res){
+	var now = new Date();
+	res.render('contest/vacation-photo', {
+		year: now.getFullYear(), month: now.getMonth()
+	})
+});
+
+app.post('/contest/vacation-photo/:year/:month', function(req, res){
+	var form = new formidable.IncomingForm();
+	form.parse(req, function(err, fields, files){
+		if (err)
+		{
+			console.log('form.parse err:' + err)
+			return res.redirect(303, '/error');
+		}
+			
+		console.log('received fields:');
+		console.log(fields);
+		console.log('received filed:');
+		console.log(files);
+		res.redirect(303, '/thank-you');
+	});
+});
+
 // custom 404 page 
 app.use(function(req, res){
 	//res.type('text/plain');
